a7992087cbde6782688b4941cd9f16758cdc91ec
coolguy129 <62524025+coolguy129@users.noreply.github.com>
2021-03-15 09:38:09 +0000

    Delete wine-chroot-2.sh

--- wine-chroot-2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-su - mobian
-echo "You need to enter your password. This is used for the 32 bit arm root account"
-passwd mobian
-printf 'export LANGUAGE="C"\nexport LC_ALL="C"\nexport DISPLAY=:0' >> ~/.bashrc
-exit
