af96c6476da641294af0b270561871c0faef3cba
coolguy129 <62524025+coolguy129@users.noreply.github.com>
2021-03-15 09:38:31 +0000

    Delete wine-chroot-4.sh

--- wine-chroot-4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Downloading wine"
-wget https://twisteros.com/wine.tgz
-echo "Extracting wine"
-tar zxvf wine.tgz
-echo "Installing wine dependencies"
-apt install -y zenity
-exit
